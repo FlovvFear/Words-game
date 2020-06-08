4ba18f7b72abbb0edd9c1c45de46e4f9466d2a9b
Artem <artnerab@gmail.com>
2020-06-09 02:57:18 +0300

    исправлены некоторые недочеты

---
+++ script.js
@@ -0,0 +1,44 @@
+let words = [
+    // "интересный",
+    // "умный",
+    "арка"
+    // "оборот",
+    // "ключ"
+];
+let word = words[Math.floor(Math.random() * words.length)];
+console.log('1:', word.length);
+
+let answerArray = [];
+for (let i = 0; i < words.length; i++) {
+    console.log(i, ':', answerArray[i]);
+    answerArray[i] = " _ ";
+}
+console.log('2:', answerArray);
+let remainingLetters = word.length;
+let leave;
+
+while (remainingLetters > 0) {
+    alert(answerArray.join(" "));
+    let guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры");
+    if (guess === null) {
+        leave = true;
+        break
+    } else if (guess.length !== 1) {
+        alert('Пожалуйста введите одну букву');
+    } else {
+        for (let i = 0; i < word.length; i++) {
+            if(guess.toLowerCase() === word[i] && guess.toLowerCase() !== answerArray[i]) {
+                answerArray[i] = guess.toLowerCase();
+                remainingLetters--;
+            }
+        }
+    }
+}
+if (leave && (remainingLetters < word.length)) {
+    alert("Вы почти справились, вот загаданное слово: " + word);
+} else if (leave) {
+    alert("Обязательно возвращайтесь");
+} else {
+    alert("Поздравляем, было загаданно слово: " +  word);
+}
+
